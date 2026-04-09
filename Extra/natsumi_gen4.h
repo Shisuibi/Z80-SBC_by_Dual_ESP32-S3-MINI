c4310b1fd50983dcdc151f911992508ea8316a56
Suibi Nominomiya <264067273+Shisuibi@users.noreply.github.com>
2026-04-10 00:51:05 +0900

    CP/M 2.2 Ready
    Minor bug fixes

--- Extra/natsumi_gen4.h
+++ Extra/natsumi_gen4.h
@@ -4,19 +4,25 @@
 // SPI_DRIVER_SELECT must be set to 0 in SdFat/SdFatConfig.h (default is 0)
 
 SdFat SD;
-#define LedS3DevKitM	48
-#define LedS3Zero		21
-#define SPIINIT	((iNeoPix == LedS3DevKitM)?12:12),\
-				((iNeoPix == LedS3DevKitM)?13:13),\
-				((iNeoPix == LedS3DevKitM)?11:11),\
-				((iNeoPix == LedS3DevKitM)? 9:10)				// sck, miso, mosi, cs
-#define SDMHZ 20
-#define SDINIT	((iNeoPix == LedS3DevKitM)? 9:10), SD_SCK_MHZ(SDMHZ)
-#define LED 0
-#define LEDinv 0
-#define BOARD "ESP32-S3-DevKitM / Zero"
-#define board_esp32
-#define board_digital_io
+
+#define		LedDevKitM	48
+#define		LedXiaoC6	15
+
+#define		SPIINIT		((iNeoPix == LedDevKitM)?12:19),\
+						((iNeoPix == LedDevKitM)?13:17),\
+						((iNeoPix == LedDevKitM)?11:20),\
+						((iNeoPix == LedDevKitM)? 9:18)		// sck, miso, mosi, cs
+
+#define		SDMHZ		20
+#define		SDINIT		((iNeoPix == LedDevKitM)? 9:18), SD_SCK_MHZ(SDMHZ)
+
+#define		LED			0
+#define		LEDinv		0
+
+#define		BOARD		"ESP32-S3-DevKitM (Slave) / XIAO ESP32C6"
+
+#define		board_esp32
+#define		board_digital_io
 
 uint8 esp32bdos(uint16 dmaaddr) {
 	return(0x00);
@@ -26,12 +32,18 @@ static uint8_t iNeoPix = LED;
 
 void NeoPixWrite(uint8_t iLedPin, uint8_t iLedBit) {
 	if(iNeoPix == LED) {
-		pinMode(iNeoPix = LedS3DevKitM, INPUT_PULLDOWN);		//	ESP32-S3-DevKitM (Slave)
-		if(digitalRead(iNeoPix) == LOW) iNeoPix = LedS3Zero;	//	ESP32-S3-Zero
+		pinMode(1, INPUT_PULLDOWN);
+
+		if(digitalRead(1) == HIGH)	iNeoPix = LedDevKitM;	// ESP32-S3-DevKitM (Slave)
+		else						iNeoPix = LedXiaoC6;	// XIAO ESP32C6
+
+		pinMode(iNeoPix, OUTPUT);
 	}
 
-	if(iLedBit == LOW)	neopixelWrite(iNeoPix, 0x00, 0x00, 0x00);
-	else				neopixelWrite(iNeoPix, 0x40, 0x00, 0x80);
+	if(iNeoPix == LedDevKitM) {
+		if(iLedBit == LOW)	neopixelWrite(iNeoPix, 0x00, 0x00, 0x00);
+		else				neopixelWrite(iNeoPix, 0x40, 0x00, 0x80);
+	} else digitalWrite(iNeoPix, iLedBit ^ 1);
 }
 
 #endif
